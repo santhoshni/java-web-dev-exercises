7624b0b3ca38140db7169c1300838da38cb4fe5f
Santhoshni <dsanthoshni@gmail.com>
2021-01-06 16:20:19 -0600

    Added areaofacircle

---
+++ src/org/launchcode/java/studios/areaofaacircle/Area.java
@@ -0,0 +1,27 @@
+package org.launchcode.java.studios.areaofaacircle;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Area {
+
+
+    public static void main(String args[]) {
+        double r;
+        double pi = 3.14, area;
+        Scanner s = new Scanner(new InputStreamReader(System.in));
+        System.out.print("Enter a radius :");
+        r = s.nextDouble();
+        if (r < 0) {
+            System.out.println(+r + " is a negative number ");
+        } else {
+            //Circle.getArea();
+            //area = pi * r* r;
+
+            System.out.println("The area of a circle of radius " + r + " is: " + Circle.getArea(r));
+        }
+
+    }
+}
+
+
---
+++ src/org/launchcode/java/studios/areaofaacircle/Circle.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.studios.areaofaacircle;
+
+public class Circle {
+
+    public static Double getArea(Double radius) {
+        return 3.14 * radius * radius;
+
+    }
+}
