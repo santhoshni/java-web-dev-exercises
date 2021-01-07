810f72ac59d58f386456be495701825f4c74affc
Santhoshni <dsanthoshni@gmail.com>
2021-01-06 22:27:20 -0600

    Added while condition

--- src/org/launchcode/java/studios/areaofaacircle/Area.java
+++ src/org/launchcode/java/studios/areaofaacircle/Area.java
@@ -7,19 +7,21 @@ public class Area {
 
 
     public static void main(String args[]) {
-        double r;
-        int i=0;
+        double radius;
         double pi = 3.14, area;
-        Scanner s = new Scanner(new InputStreamReader(System.in));
+        //Scanner s = new Scanner(new InputStreamReader(System.in));
+        Scanner s = new Scanner(System.in);
         System.out.print("Enter a radius :");
-        r = s.nextDouble();
-        if (r < 0) {
-            System.out.println(+r + " is a negative number ");
-        } else {
-            //Circle.getArea();
-            //area = pi * r* r;
 
-            System.out.println("The area of a circle of radius " + r + " is: " + Circle.getArea(r));
+        boolean flags = true;
+        while(flags){
+            radius = s.nextDouble();
+            if (radius < 0) {
+                System.out.println(+radius + " is a negative number ");
+            } else {
+                System.out.println("The area of a circle of radius " + radius + " is: " + Circle.getArea(radius));
+                flags = false;
+            }
         }
 
     }
